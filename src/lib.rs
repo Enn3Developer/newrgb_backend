3fa04eb5c1f94f3ab91efdc26382dbbc80b533f9
enn3 <arturpeshko39@gmail.com>
2024-03-20 21:15:20 +0100

    better management

---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+use actix_web::rt;
+use std::ffi::OsStr;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Seek, Write};
+use std::path::Path;
+
+use walkdir::{DirEntry, WalkDir};
+use zip::write::FileOptions;
+use zip::CompressionMethod;
+
+async fn zip_dir<T, P: AsRef<Path>>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: P,
+    writer: T,
+    method: CompressionMethod,
+) -> zip::result::ZipResult<()>
+where
+    T: Write + Seek,
+    P: AsRef<OsStr>,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(Path::new(&prefix)).unwrap();
+        if path.is_file() {
+            #[allow(deprecated)]
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            #[allow(deprecated)]
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
+}
+
+pub async fn zip_all<P: AsRef<Path> + AsRef<OsStr>>(dir_path: P) {
+    let file = File::create("new.zip").unwrap();
+    let walk_dir = WalkDir::new(&dir_path);
+    let it = walk_dir.into_iter();
+    zip_dir(
+        &mut it.filter_map(|e| e.ok()),
+        dir_path,
+        file,
+        CompressionMethod::Zstd,
+    )
+    .await
+    .unwrap();
+}
+
+pub async fn generate_zip() {
+    rt::spawn(async {
+        zip_all("data").await;
+        fs::rename("new.zip", "newrgb.zip").unwrap();
+    })
+    .await
+    .expect("zip_all failed");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use actix_files::NamedFile;
-use actix_web::{get, App, HttpServer, Result};
+use actix_web::{get, App, HttpServer, Responder, Result};
 use std::io;
+use std::path::Path;
 
 #[get("/")]
 async fn index() -> Result<NamedFile> {
+    if !Path::new("newrgb.zip").exists() {
+        newrgb_backend::generate_zip().await
+    }
     Ok(NamedFile::open("newrgb.zip")?)
+}
+
+#[get("/generate_zip")]
+async fn generate_zip() -> impl Responder {
+    newrgb_backend::generate_zip().await;
+    "Done"
 }
 
 #[actix_web::main]
@@ -14,7 +24,7 @@ async fn main() -> io::Result<()> {
     } else {
         "0.0.0.0"
     };
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(generate_zip))
         .bind((ip, 6969))?
         .run()
         .await
